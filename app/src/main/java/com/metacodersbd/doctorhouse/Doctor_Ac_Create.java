fb13cea413cf96513f8cd7f40b35b5100c755094
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2019-09-09 03:55:14 +0600

    qq

---
+++ app/src/main/java/com/metacodersbd/doctorhouse/Doctor_Ac_Create.java
@@ -0,0 +1,14 @@
+package com.metacodersbd.doctorhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Doctor_Ac_Create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor__ac__create);
+    }
+}
