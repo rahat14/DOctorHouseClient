4d98084780b195d403dd7c1975a248d259580454
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2019-09-09 02:12:19 +0600

    nn
    nn

---
+++ app/src/main/java/com/metacodersbd/doctorhouse/Row_Hospital.java
@@ -0,0 +1,14 @@
+package com.metacodersbd.doctorhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Row_Hospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_row__hospital);
+    }
+}
